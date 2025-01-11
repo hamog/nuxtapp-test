faeb03ebd60aec97b9da1ffb3fcf03695dabc4ce
hashem <hashemm364@gmail.com>
2025-01-11 21:20:08 +0330

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <AppAlert>
+      This is an auto-imported component.
+    </AppAlert>
+    <img src="/public/img/click-150.jpg" alt="" width="300" height="200">
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
