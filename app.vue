1583251889ca0fcc9f6e034fde1e2651fb27816e
hamog <hashemm364@gmail.com>
2025-01-13 11:44:58 +0330

    Add some features

--- app.vue
+++ app.vue
@@ -1,10 +1,9 @@
+<style>
+  @import url("~/assets/css/main.css");
+</style>
+
 <template>
   <div>
-    <AppAlert>
-      This is an auto-imported component.
-    </AppAlert>
-    <img src="/public/img/click-150.jpg" alt="" width="300" height="200">
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Home page</h1>
   </div>
 </template>
